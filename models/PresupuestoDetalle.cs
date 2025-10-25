d48129f475fee8a0a8b7df7eced352cf8277a95c
Paulo Srur <paulosrur777@gmail.com>
2025-10-25 20:56:19 -0300

    creo modelo de presupuestoDetalles

---
+++ models/PresupuestoDetalle.cs
@@ -0,0 +1,16 @@
+namespace TP7.Models
+{
+    public class PresupuestoDetalle
+    {
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+
+        public PresupuestoDetalle() { }
+
+        public PresupuestoDetalle(Producto producto, int cantidad)
+        {
+            Producto = producto;
+            Cantidad = cantidad;
+        }
+    }
+}
