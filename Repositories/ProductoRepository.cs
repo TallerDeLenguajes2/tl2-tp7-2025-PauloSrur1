a80908417134c2c1c1e7b18945c42e7f2e3398b7
Paulo Srur <paulosrur777@gmail.com>
2025-10-25 21:47:05 -0300

    logica de funcion para crear un nuevo  producto

--- Repositories/ProductoRepository.cs
+++ Repositories/ProductoRepository.cs
@@ -7,5 +7,26 @@ namespace Repositories
     public class ProductoRepository
     {
         private string cadenaConexion = "Data Source=Tienda.db;";
+
+        //logica para crear un nuevo producto 
+        public void Crear(Producto producto)
+        {
+            using var conexion = new SqliteConnection(cadenaConexion);
+            conexion.Open();
+
+            string sql = "INSERT INTO Productos (descripcion, precio) VALUES (@desc, @precio)";
+            using var comando = new SqliteCommand(sql,conexion);
+            comando.Parameters.AddWithValue("@desc", producto.Descripcion);
+            comando.Parameters.AddWithValue("@precio", producto.Precio);
+            comando.ExecuteNonQuery();
+        }
+
+
+
+
+
+
+
+
     }
 }
