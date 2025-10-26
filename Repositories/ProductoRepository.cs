0f5536140173931059166790211ee1512cc987c2
Paulo Srur <paulosrur777@gmail.com>
2025-10-25 21:42:09 -0300

    creando repositorio de producto

---
+++ Repositories/ProductoRepository.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using Models;
+
+namespace Repositories
+{
+    public class ProductoRepository
+    {
+        private string cadenaConexion = "Data Source=Tienda.db;";
+    }
+}
