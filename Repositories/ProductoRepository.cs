a045348b8265fe101db000434f9b4e2d9aeedb8e
Paulo Srur <paulosrur777@gmail.com>
2025-10-25 21:49:29 -0300

    logica de funcion para modificar un producto

--- Repositories/ProductoRepository.cs
+++ Repositories/ProductoRepository.cs
@@ -21,6 +21,19 @@ namespace Repositories
             comando.ExecuteNonQuery();
         }
 
+        //logica para modificar un producto existente
+        public void Modificar(Producto producto)
+        {
+            using var conexion = new SqliteConnection(cadenaConexion);
+            conexion.Open();
+
+            string sql = "UPDATE Productos SET descripcion = @desc, precio = @precio WHERE idProducto = @id";
+            using var comando = new SqliteCommand(sql, conexion);
+            comando.Parameters.AddWithValue("@desc", producto.Descripcion);
+            comando.Parameters.AddWithValue("@precio", producto.Precio);
+            comando.Parameters.AddWithValue("@id", producto.IdProducto);
+            comando.ExecuteNonQuery();
+        }
 
 
 
