673390bd9077671ca3cf31ef2bc4ed4439d03f36
PauloSrur1 <psrr71@gmail.com>
2025-10-31 11:36:26 -0300

    controlador de presupuesto creado

---
+++ Controllers/PresupuestoController.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using Models;
+using Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PresupuestoContoller : ControlleBase
+    {
+        private readonly PresupuestoRepository _repo;
+        public PresupuestoController()
+        {
+            _repo = new PresupuestoRepository();        
+        } 
+
+        [HttpPost]
+        public IActionResult CrearPresupuesto([FromBody] Presupuesto nuevo)
+        {
+            _repo.Crear(nuevo);
+            return Created("", "Presupuesto creado correctamente");
+        }
+
+        [HttpPost("{id}/ProductoDetalle")]
+        public IActionResult AgregarProducto(int id, [FromQuery] int idProducto, [FromQuery] int cantidad)
+        {
+            _repo.AgregarProducto(id,idProducto,cantidad);
+            return Ok("Producto agregado al presupuesto");
+        }
+
+        [HttpGet]
+        public ActionResult<List<Presupuesto>> ListarPresupuestos()
+        {
+            var lista = _repo.Listar();
+            return Ok(lista);
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Presupuesto> ObtenerPorId(int id)
+        {
+            var presupuesto = _repo.ObtenerPorId(id);
+            if(presupuesto = null)
+            {
+                return NotFound($"No se encontro el presupuesto con ID {id}");
+                return Ok(presupuesto);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult EliminarPresupuesto(int id)
+        {
+            bool eliminado = _repo.Eliminar(id);
+            if(eliminado)
+            {
+                return NoContent();
+            }else
+            {
+                return NotFound($"No se encontro el presupuesto con ID {id}");
+            }
+        }
+
+    }
+}
--- obj/Debug/net9.0/tl2-tp7-2025-PauloSrur1.AssemblyInfo.cs
+++ obj/Debug/net9.0/tl2-tp7-2025-PauloSrur1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp7-2025-PauloSrur1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2966159ab2fe564ef7e5466f80e5add86b2303e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1812beb1baa49617e04d289598c34eebe1b0ac81")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp7-2025-PauloSrur1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp7-2025-PauloSrur1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
